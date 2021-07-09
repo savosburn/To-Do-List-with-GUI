06eee234a429f862e75a22c7fe6a241dcc1c0d2c
savosburn <savosburn18@tampabay.rr.com>
2021-07-09 17:17:26 -0400

    removed warnings from InvalidDateControler .java, .fxml

--- src/main/java/ucf/assignments/InvalidDateController.java
+++ src/main/java/ucf/assignments/InvalidDateController.java
@@ -1,29 +1,25 @@
 package ucf.assignments;
 
-import java.net.URL;
-import java.util.Objects;
-import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
+//import java.net.URL;
+//import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class InvalidDateController {
 
+    /*
     @FXML
     private ResourceBundle resources;
 
     @FXML
-    private URL location;
+    private URL location;*/
 
     @FXML
     private Button okButton;
 
     @FXML
-    public String okButtonPressed(ActionEvent event) {
+    public String okButtonPressed() {
 
         try {
             Stage curStage = (Stage)okButton.getScene().getWindow();
