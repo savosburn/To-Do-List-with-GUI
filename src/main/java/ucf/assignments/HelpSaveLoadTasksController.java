05ac6d2f4b022aa5babd4b200ea3e7adf2f93f04
savosburn <savosburn18@tampabay.rr.com>
2021-07-10 22:52:50 -0400

    Sort help completed

--- src/main/java/ucf/assignments/HelpSaveLoadTasksController.java
+++ src/main/java/ucf/assignments/HelpSaveLoadTasksController.java
@@ -1,4 +1,39 @@
 package ucf.assignments;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class HelpSaveLoadTasksController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button returnButton;
+
+    @FXML
+    private Button nextButton;
+
+    @FXML
+    void nextButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void returnButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert returnButton != null : "fx:id=\"returnButton\" was not injected: check your FXML file 'HelpSaveLoadTasksController.fxml'.";
+        assert nextButton != null : "fx:id=\"nextButton\" was not injected: check your FXML file 'HelpSaveLoadTasksController.fxml'.";
+
+    }
 }
